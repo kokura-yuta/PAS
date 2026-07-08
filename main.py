a5cb0e2444fbde0126079757e34db3e7fa373733
kokura-yuta <y0u2t1a8@icloud.com>
2026-07-08 11:54:53 +0900

    First commit

---
+++ main.py
@@ -0,0 +1,108 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from datetime import datetime
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+class ChatMessage(Base):
+    __tablename__="chat_messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    role = Column(String(20))
+    content = Column(Text)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
+Base.metadata.create_all(bind=engine)
+
+def save_message(role, content):
+    db = SessionLocal()
+
+    new_message = ChatMessage(
+        role=role,
+        content=content
+    )
+
+    db.add(new_message)
+    db.commit()
+    db.close()
+
+def load_messages():
+    db = SessionLocal()
+
+    messages = db.query(ChatMessage).order_by(ChatMessage.created_at).all()
+
+    history =""
+
+    for msg in messages:
+        history += f"{msg.role}: {msg.content}\n"
+    
+    db.close()
+
+    return history
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="home.html",
+        context={}
+    )
+
+@app.get("/chat")
+def chat_page(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="chat.html"
+    )
+@app.post("/chat")
+def chat_send(request: Request, message: str = Form(...)):
+    save_message("user", message)
+    response = client.responses.create(
+        model="gpt-4.1-mini",
+        input=f"""
+あなたはPASです。
+友達のように自然で、相談しやすい口調で返して下さい。
+説教っぽくせずに、相手に寄り添って下さい。
+
+ユーザーの発言:
+{message}
+"""
+    )
+
+    ai_message = response.output_text
+
+    save_message("assistant", ai_message)
+    
+    return templates.TemplateResponse(
+        request=request,
+        name="chat.html",
+        context={
+            "message": message,
+            "ai_message": ai_message
+            }
+    )
